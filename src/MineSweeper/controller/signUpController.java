1a337af9958598e0ef244d872d3843b4b0452ecb
Matthew <matthewbarthelmes@gmail.com>
2022-04-27 08:34:06 -0400

    MS3 Update
    This build includes everything shown in MS3 except for the JUnit tests.

--- src/MineSweeper/controller/signUpController.java
+++ src/MineSweeper/controller/signUpController.java
@@ -6,12 +6,20 @@ public class signUpController {
 	private String username, password, passwordCheck ;
 	
 	public boolean checkSignUp(String username, String password, String passwordCheck ) {
+		boolean goodSignUp = false;
 		//System.out.println(username + ", " + password);
-				if(password.equals(passwordCheck)) {
-					return true;
-				}
-				else {
-					return false;
-				}
+		if(password.equals(passwordCheck)) {
+			this.username = username;
+			this.password = password;
+			DerbyDatabase derbyDB = new DerbyDatabase();
+			boolean userExists = derbyDB.checkUsernameExists(username);
+			if(userExists == false) {
+				derbyDB.insertUserIntoUsersTable(username, password);
+				goodSignUp = true;
+				System.out.println("User already exists.");
+			}
+		}
+		
+		return goodSignUp;
 	}
 }
